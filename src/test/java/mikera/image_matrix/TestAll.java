86a451fcaafee397ccac82b3b004c289713c1a84
mikera <mike.r.anderson.13@gmail.com>
2013-02-03 13:31:13 +0800

    Add cljunit testing

---
+++ src/test/java/mikera/image_matrix/TestAll.java
@@ -0,0 +1,11 @@
+package mikera.image_matrix;
+
+import mikera.cljunit.ClojureTest;
+
+public class TestAll extends ClojureTest {
+
+	@Override
+	public String filter() {
+		return "mikera.image-matrix";
+	}
+}
