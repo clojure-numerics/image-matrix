831117a59d0817a365a301b5ba2c975481b12f0b
mikera <mike.r.anderson.13@gmail.com>
2013-08-23 16:18:41 +0800

    add image constructor function

--- src/main/clojure/mikera/image_matrix.clj
+++ src/main/clojure/mikera/image_matrix.clj
@@ -6,3 +6,8 @@
   "Creates a new BufferedImage of the specified with and hight (in pixels)" 
   ^java.awt.image.BufferedImage [width height]
   (mikera.image-matrix.impl/new-image (long width) (long height)))
+
+(defn image 
+  "Creates a new BuferedImage from the given data"
+  ([data]
+    (array :buffered-image data)))
